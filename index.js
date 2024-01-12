cf74fb0f1186ef5fa85dd5b2e06a46ba55104924
Sheila Hanson <slhanson11@live.com>
2024-01-12 12:17:48 -0600

    added tri test. cir/sq next

--- index.js
+++ index.js
@@ -10,3 +10,10 @@
 // TODO: output text "Generated logo.svg" is printed to command line
 // TODO: open 'logo.svg' file in browser
 // TODO: I'm shown a 300 x 200 pixel img that matches criteria
+
+/* Example SVG:
+<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200">
+  <circle cx="150" cy="100" r="80" fill="green"/>
+  <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
+</svg>
+*/
