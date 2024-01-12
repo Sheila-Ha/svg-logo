c66342244e5774d60070763bc468989501a4b491
Sheila Hanson <slhanson11@live.com>
2024-01-12 10:35:34 -0600

    added 'todo list'

---
+++ index.js
@@ -0,0 +1,12 @@
+// TODO: prompted for Text
+// TODO: enter up to 3 char
+// TODO: prompted for text color
+// TODO: enter color keyword or hexidec
+// TODO: prompted for a shape
+// TODO: presented w/ a list of shapes (cir/tri/sq)
+// TODO: prompted for shapes color
+// TODO: enter color keyword or hexidec
+// TODO: all prompts done = svg file is created named 'logo.svg'
+// TODO: output text "Generated logo.svg" is printed to command line
+// TODO: open 'logo.svg' file in browser
+// TODO: I'm shown a 300 x 200 pixel img that matches criteria
